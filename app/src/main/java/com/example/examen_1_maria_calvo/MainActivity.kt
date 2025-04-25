6e5a076f0f97012621ebad36da2071075a08f841
mariacg <merycalvo98@hotmail.com>
2025-04-25 10:23:05 +0200

    base

--- app/src/main/java/com/example/examen_1_maria_calvo/MainActivity.kt
+++ app/src/main/java/com/example/examen_1_maria_calvo/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.examen_1_maria_calvo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var calculateButton: Button
+
+    lateinit var resultTextView: TextView
+    lateinit var gastosTextView: TextView
+    lateinit var ingresosTextView: TextView
+
+
+    lateinit var weightAddButton: Button
+
+    var ingresos = 170.0f
+    var gastos = 75.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        calculateButton.setOnClickListener {
+            calcularBalance()
+        }
+
     }
+
+    private fun calcularBalance(){
+        var result = ingresos - gastos
+
+        resultTextView.text = String.format(result.toString())
+    }
+
+    fun initViews(){
+
+        calculateButton = findViewById(R.id.calculateButton)
+        resultTextView = findViewById(R.id.resultTextView)
+        gastosTextView = findViewById(R.id.resultTextView)
+    }
+
 }
